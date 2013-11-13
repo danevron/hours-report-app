9ec01953143c60df2067b19d8ea60bb17ce62cdc
Dan Evron <dan.evron@klarna.com>
2013-11-14 00:43:38 +0200

    Creating a Report active record model to represent the monthly report

---
+++ db/migrate/20131113222046_create_reports.rb
@@ -0,0 +1,12 @@
+class CreateReports < ActiveRecord::Migration
+  def change
+    create_table :reports do |t|
+      t.datetime :start_date
+      t.datetime :end_date
+      t.string :status
+      t.boolean :current
+
+      t.timestamps
+    end
+  end
+end
