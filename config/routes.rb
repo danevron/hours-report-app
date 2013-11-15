3ca506dd7f23e49fb1d876f7e252f5babc8ff5d3
Dan Evron <dan.evron@klarna.com>
2013-11-15 23:53:26 +0200

    added reports routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 HoursReport::Application.routes.draw do
 
-  resources :invitations, only: [:new, :create]
-  resources :users, only: [:index, :show]
   root 'users#show'
 
+  resources :invitations, only: [:new, :create]
+  resources :users,       only: [:index, :show]
+  resources :reports,     only: [:index, :show, :new, :create]
+
   match '/auth/:provider/callback' => 'sessions#create', via: %i(get post)
-  match '/logout' => 'sessions#destroy', via: %i(get delete), as: :logout
-  match '/login' => 'sessions#new', via: %i(get), as: :login
+  match '/logout'                  => 'sessions#destroy', via: %i(get delete), as: :logout
+  match '/login'                   => 'sessions#new', via: %i(get), as: :login
 end
