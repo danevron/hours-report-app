8ea7142e4eb7d4e7a68cf09ecb46e6576e557952
Dan Evron <dan.evron@klarna.com>
2013-11-23 18:08:45 +0200

    added routes to edit and update user_report

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,14 @@ HoursReport::Application.routes.draw do
 
   root 'users#show'
 
-  resources :invitations, only: [:new, :create]
-  resources :users,       only: [:index, :show]
-  resources :reports,     only: [:index, :show, :new, :create, :destroy]
+  resources :invitations,    only: [:new, :create]
+  resources :users,          only: [:index, :show] do
+    resources :user_reports, only: [:edit, :update]
+  end
+
+  resources :reports,        only: [:index, :show, :new, :create, :destroy] do
+    resources :user_reports, only: [:edit, :update]
+  end
 
   match '/auth/:provider/callback' => 'sessions#create', via: %i(get post)
   match '/logout'                  => 'sessions#destroy', via: %i(get delete), as: :logout
