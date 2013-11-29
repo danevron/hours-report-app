0031221ebe50318e7c020683ec2e956c0d4c8d85
Dan Evron <dan.evron@klarna.com>
2013-11-29 17:12:21 +0200

    Add default values to a day according to the weekday

--- app/models/day.rb
+++ app/models/day.rb
@@ -1,6 +1,8 @@
 class Day < ActiveRecord::Base
   belongs_to :timesheet
 
+  before_create :prefill_default_values
+
   def self.build_days(from, to)
     (from.to_date..to.to_date).map do |date|
       new(:date => date)
@@ -13,5 +15,17 @@ class Day < ActiveRecord::Base
 
   def weekend?
     weekday == "Friday" || weekday == "Saturday"
+  end
+
+  private
+
+  def prefill_default_values
+    if weekend?
+      self.day_type = "weekend"
+      self.value = 0
+    else
+      self.day_type = "work_day"
+      self.value = 9
+    end
   end
 end
