f5849fd200a900e4ee166295d94ba281325f9639
Dan Evron <dan.evron@klarna.com>
2013-12-07 23:25:42 +0200

    add refresh access_token functionality for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,7 +19,7 @@ class User < ActiveRecord::Base
       provider:      auth["provider"],
       refresh_token: auth["credentials"]["refresh_token"],
       access_token:  auth["credentials"]["token"],
-      expires:       auth["credentials"]["expires_at"],
+      expires:       auth["credentials"]["expires_in"],
       first_name:    auth["info"]["first_name"],
       last_name:     auth["info"]["last_name"],
       image:         auth["info"]["image"],
@@ -35,7 +35,42 @@ class User < ActiveRecord::Base
     current_report.timesheets.find_by(:user_id => id) if current_report
   end
 
+
+  def access_token_for_api
+    refresh_access_token! unless token_is_valid?
+    self.access_token
+  end
+
   private
+
+  def token_is_valid?
+    token_validation[:audience] == ENV['GOOGLE_KEY']
+  end
+
+  def refresh_access_token!
+    body = {
+      'refresh_token' => self.refresh_token,
+      'client_id'     => ENV['GOOGLE_KEY'],
+      'client_secret' => ENV['GOOGLE_SECRET'],
+      'grant_type'    => 'refresh_token'
+    }
+    response = Typhoeus.post("https://accounts.google.com/o/oauth2/token", body: body)
+    user_tokens = JSON.parse(response.body)
+
+    self.access_token = user_tokens['access_token']
+    self.expires = user_tokens['expires_in']
+    save
+  end
+
+  def token_validation
+    @token_validation ||= begin
+      options  = {
+        :params => { :access_token => self.access_token }
+      }
+      response = Typhoeus.get("https://www.googleapis.com/oauth2/v1/tokeninfo", options)
+      HashWithIndifferentAccess.new(JSON.parse(response.body))
+    end
+  end
 
   def join_current_report
     current_report.add_new_user(self)
