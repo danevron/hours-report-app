d16336b0e505b1b64428ca43d6d63314c34d9e5b
Dan Evron <dan.evron@klarna.com>
2013-12-14 21:42:44 +0200

    add redirection back or to the root path in case the user is not authorized to manage a resource

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,8 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
 
   def access_denied
-    return render(text: 'access_denied: requires a role')
+    flash[:alert] = 'You are not authorized to view this page!'
+    redirect_to_back_or_default
   end
 
   alias_method :role_access_denied, :access_denied
@@ -22,5 +23,13 @@ class ApplicationController < ActionController::Base
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def redirect_to_back_or_default(default = root_url)
+    if request.env["HTTP_REFERER"].present? and request.env["HTTP_REFERER"] != request.env["REQUEST_URI"]
+      redirect_to :back
+    else
+      redirect_to default
+    end
   end
 end
