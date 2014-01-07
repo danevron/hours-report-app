9b6c6a1263182b33e669cce2b207d43ccc3918c8
Dan Evron <dan.evron@klarna.com>
2014-01-07 20:27:39 +0200

    not using gmail to send emails

--- config/application.rb
+++ config/application.rb
@@ -29,13 +29,5 @@ module HoursReport
     # config.i18n.default_locale = :de
 
     config.action_mailer.delivery_method = :smtp
-    config.action_mailer.smtp_settings = {
-      :address              => 'smtp.gmail.com',
-      :port                 => 587,
-      :user_name            => ENV['GMAIL_USERNAME'],
-      :password             => ENV['GMAIL_PASSWORD'],
-      :authentication       => 'plain',
-      :enable_starttls_auto => true
-    }
   end
 end
