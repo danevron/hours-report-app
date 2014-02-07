0a57d6ac64236ef2778845dc08dd37a419945244
Dan Evron <dan.evron@klarna.com>
2014-02-07 17:54:53 +0200

    fix unadded fonts

--- config/application.rb
+++ config/application.rb
@@ -28,6 +28,7 @@ module HoursReport
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
     config.action_mailer.delivery_method = :smtp
   end
 end
