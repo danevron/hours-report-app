3ee2d9d2a9e2b84ad1d3ca39c731673d1a5d3e30
Dan Evron <danevron@Dans-MacBook-Pro-2.local>
2014-03-13 23:59:51 +0200

    change production host to hours.klarnaisrael.com

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,7 +89,7 @@ HoursReport::Application.configure do
   }
 
 
-  config.action_mailer.default_url_options = { :host => 'hours-report.herokuapp.com' }
-  config.action_mailer.asset_host          = "http://hours-report.herokuapp.com/"
-  ActionMailer::Base.default :from => 'Hours Report <noreply@hoursreport.com>'
+  config.action_mailer.default_url_options = { :host => 'hours.klarnaisrael.com' }
+  config.action_mailer.asset_host          = "http://hours.klarnaisrael.com/"
+  ActionMailer::Base.default :from => 'Hours Report <noreply@hours.klarnaisrael.com>'
 end
