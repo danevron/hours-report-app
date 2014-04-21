b243a97ceeb16d5fde59351e6be6706b48a8a5e6
Dan Evron <danevron@gmail.com>
2014-04-21 10:59:14 +0300

    WIP - adds a view for creating a new expense report

--- app/controllers/expense_reports_controller.rb
+++ app/controllers/expense_reports_controller.rb
@@ -1,16 +1,27 @@
 class ExpenseReportsController < ApplicationController
+  before_action :load_user
+  before_action :login_required
+
   def new
+    @expense_report = expense_reports.build_report
   end
 
   def create
   end
 
   def index
+    expense_reports.all
   end
 
   def edit
   end
 
   def update
+  end
+
+  private
+
+  def expense_reports
+    @user ? @user.expense_reports : ExpenseReport
   end
 end
