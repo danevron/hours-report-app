f3263c4dd6aed4a8602049fa3b5e830fafb56729
Dan Evron <danevron@gmail.com>
2014-05-24 22:33:30 +0300

    having the api respond to JSON

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -1,3 +1,11 @@
 class Api::V1::ApiController < ApplicationController
+  respond_to :json
 
+  before_filter :set_default_response_format
+
+  protected
+
+  def set_default_response_format
+    request.format = :json if params[:format].blank?
+  end
 end
