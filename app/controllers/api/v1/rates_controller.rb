0a823073a455e5a46d012515a2b72c597bbe5919
Dan Evron <danevron@gmail.com>
2014-06-21 21:37:35 +0300

    changes the rates controller to return all the exchange rates

--- app/controllers/api/v1/rates_controller.rb
+++ app/controllers/api/v1/rates_controller.rb
@@ -1,25 +1,45 @@
 class Api::V1::RatesController < Api::V1::ApiController
 
   def index
-    render :json => get_rates(params[:currency].downcase.to_sym, Date.parse(params[:start_date]), Date.parse(params[:end_date]))
+    render :json => get_rates(Date.parse(params[:start_date]), Date.parse(params[:end_date]))
   end
 
   private
 
-  def get_rates(currency, start_date, end_date)
-    if currency == :ils
-      { rate: 1 }
-    else
-      rates = []
-      start_date.upto(end_date) do |date|
-        begin
-          returned_rates_data = BankOfIsrael.rates(date)
-          rates << (returned_rates_data[currency][:rate].to_d / returned_rates_data[currency][:unit].to_d)
-        rescue RuntimeError
+  def get_rates(start_date, end_date)
+    start_date.upto(end_date) do |date|
+      begin
+        returned_rates_data = BankOfIsrael.rates(date)
+        if returned_rates_data.delete(:release_date)
+          returned_rates_data.each do |currency, data|
+            calculated_rate = (data[:rate].to_d / data[:unit].to_d)
+            rates[currency.to_s.upcase] = calculated_rate unless rates[currency.to_s.upcase] > calculated_rate
+          end
         end
+      rescue RuntimeError
       end
-
-      { rate: rates.max }
     end
+
+    rates
+  end
+
+  def rates
+    @rates ||= {
+      "ILS" => 1,
+      "USD" => 0,
+      "GBP" => 0,
+      "JPY" => 0,
+      "EUR" => 0,
+      "AUD" => 0,
+      "CAD" => 0,
+      "DKK" => 0,
+      "NOK" => 0,
+      "ZAR" => 0,
+      "SEK" => 0,
+      "CHF" => 0,
+      "JOD" => 0,
+      "EGP" => 0,
+      "LBP" => 0
+    }
   end
 end
