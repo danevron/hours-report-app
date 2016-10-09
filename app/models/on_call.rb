6ce8186d645f1a1d98143b21b7c497f82edcaeee
Lior Goldhamer <lior.goldhamer@klarna.com>
2016-10-09 12:18:55 +0300

    update end time for on call extraction

--- app/models/on_call.rb
+++ app/models/on_call.rb
@@ -3,7 +3,7 @@ class OnCall < ActiveRecord::Base
   def self.update_period(start_date, end_date)
     Time.zone = "Asia/Jerusalem"
     start_time = Time.zone.parse(start_date.to_s) + (10.5).hours
-    end_time = Time.zone.parse(end_date.to_s) + (10.5).hours
+    end_time = Time.zone.parse(end_date.to_s) + 1.day + (10.5).hours # we want to include the last day of the month too
     on_call_schedules = OpsgenieClient.extract_final_schedule(start_time, end_time)
 
     on_call_schedules.each do |schedule, on_call_days|
